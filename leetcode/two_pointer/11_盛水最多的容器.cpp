7a1d4cad3ccb610b84edacb4982afb4e18732448
y5023677-spec <y5023677@gmail.com>
2026-09-18 11:17:31 +0800

    add leetcode 11_盛水最多的容器，two pointer

---
+++ leetcode/two_pointer/11_盛水最多的容器.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int ans = 0;
+        while (left < right) {
+            int s = (right - left) * min(height[left], height[right]);
+            ans = max(ans, s);
+            height[left] <= height[right] ? left++ : right--;
+        }
+        return ans;
+    }
+};
